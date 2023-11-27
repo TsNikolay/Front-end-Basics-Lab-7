f7bb9fb31ef4f794f82e11de95306dd2bd0e4562
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-11-27 23:44:20 +0100

    Lab7. Second task was implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Content from "./components/Content";
+import Content from "./components/Task 1/Content";
+import GoodsList from "./components/Task 2/GoodsList";
 
 function App() {
   return (
     <div className="App">
+      <h1>Завдання 1</h1>
       <Content />
+      <h1>Завдання 2</h1>
+      <GoodsList />
     </div>
   );
 }
